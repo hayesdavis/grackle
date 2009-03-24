b6375eeae1c9009af52a62f26207d81918948c8c
Hayes Davis <hayes@appozite.com>
2009-03-23 21:17:47 -0500

    Tweaked gem version number

--- lib/grackle.rb
+++ lib/grackle.rb
@@ -1,7 +1,7 @@
 module Grackle
 
   # :stopdoc:
-  VERSION = '0.0.2'
+  VERSION = '0.0.3'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   # :startdoc:
