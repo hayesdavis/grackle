0154a8f567ccd06986a21317174d9c6f393e05ec
Hayes Davis <hayes@appozite.com>
2012-05-13 16:27:24 -0700

    Extract version out a separate file

--- lib/grackle.rb
+++ lib/grackle.rb
@@ -1,18 +1,3 @@
-module Grackle
-
-  # :stopdoc:
-  VERSION = '0.1.10'
-  LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
-  PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
-  # :startdoc:
-
-  # Returns the version string for the library.
-  def self.version
-    VERSION
-  end
-
-end  # module Grackle
-
 $:.unshift File.dirname(__FILE__)
 
 require 'ostruct'
@@ -25,6 +10,7 @@ require 'oauth'
 require 'oauth/client'
 require 'mime/types'
 
+require 'grackle/version'
 require 'grackle/utils'
 require 'grackle/transport'
 require 'grackle/handlers'
---
+++ lib/grackle/version.rb
@@ -0,0 +1,9 @@
+module Grackle
+
+  VERSION = "0.1.12"
+
+  def self.version
+    VERSION
+  end
+
+end
